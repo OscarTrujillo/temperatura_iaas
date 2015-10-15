eca1e4edcb357e1d26d2e5bd23b0041d98bf6e68
OscarTrujillo <oscartrujillo94@gmail.com>
2015-10-15 17:47:53 +0000

    herencia terminada

--- temperature.js
+++ temperature.js
@@ -1,13 +1,34 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function Media(num,cad) {
   
-   this.numero=num;
-   this.cadena=cad;
-
+   var numero=num;
+   var cadena=cad;
+    
+  this.getn =function(){
+    return numero
+  }
+  this.getc =function(){
+    return cadena
+  }
 }
 
 function Temperatura (num, cad) {
-   Media.call(num, cad);
+    Media.call(this, num, cad);
+    var type= this.getc();
+    var result;
+    
+   ///
+    var number = parseFloat(this.getn());
+    if (type == 'c' || type == 'C') {
+      result = (number * 9/5)+32;
+      result = result.toFixed(1)+" Farenheit"
+    }
+    else {
+      result = (number - 32)*5/9;
+      result = result.toFixed(1)+" Celsius"
+    }
+    converted.innerHTML = result;
+   ///
 }
 
 
@@ -22,23 +43,16 @@ function calculate() {
   
   if (m) {
     
-    var result;
     var num = m[1]; 
     var type = m[2];
-
+    
+      Temperatura.prototype = new Media;
+      a = new Temperatura(num,type);
    
-   var num = parseFloat(num);
-    if (type == 'c' || type == 'C') {
-      result = (num * 9/5)+32;
-      result = result.toFixed(1)+" Farenheit"
-    }
-    else {
-      result = (num - 32)*5/9;
-      result = result.toFixed(1)+" Celsius"
-    }
-    converted.innerHTML = result;
+  
   }
   else {
     converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
   }
+
 }
